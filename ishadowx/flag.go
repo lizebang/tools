8cbdc2bee69c724da1fee141bc85a8b738a53b69
lizebang <lizebang1998@gmail.com>
2018-03-29 21:31:41 +0800

    add help

--- ishadowx/flag.go
+++ ishadowx/flag.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
+)
+
+const (
+	help = `%s usage:
+%s [ -h | -H | --help ]
+%s [path to store file]
+`
 )
 
 func getPath() string {
@@ -15,6 +23,10 @@ func getPath() string {
 		}
 		return path
 	} else if len(args) == 2 {
+		if isHelp(args[1]) {
+			fmt.Fprintf(os.Stdout, help, args[0], args[0], args[0])
+			os.Exit(0)
+		}
 		path, err := filepath.Abs(args[1])
 		if err != nil {
 			panic(err)
@@ -23,4 +35,11 @@ func getPath() string {
 		return path
 	}
 	panic(errors.New("too many arguments"))
+}
+
+func isHelp(s string) bool {
+	if s == "-h" || s == "-H" || s == "--help" {
+		return true
+	}
+	return false
 }
